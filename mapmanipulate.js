3782e4d0f9235c8502b43bd9fe9740e4250df3ab
Atis Mohanty <atismohanty8711@gmail.com>
2018-02-28 12:15:11 +0530

    Update mapmanipulate.js

--- mapmanipulate.js
+++ mapmanipulate.js
@@ -111,7 +111,7 @@ for (var i=0 ; i< arr.length-1 ; i++)
 					}
 					infowindow[i] = new google.maps.InfoWindow
 						({
-						content:"<div style='width:220px;height:260px; border:none'>"+
+						content:"<div style='width:240px;height:240px; border:none'>"+
 								"<p style='font-size:16px'><I><b>Company: "+ markerContent[i] + "</I></b></br>" +
 								"Address: "+ data[i].custAdd + "</br>" +
 								"City: "+ data[i].custCity + "</br>" +
